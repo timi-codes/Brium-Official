59a376093fcd4880993f253f2313f2d6e37e4f37
timi-codes <tejumoladavid@gmail.com>
2019-06-04 10:43:09 +0100

    make safety and ride component modular

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/codepreneur/WebstormProjects/Brium-Official/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/codepreneur/WebstormProjects/Brium-Official/src/pages/404.js"))),
   "component---src-pages-fare-estimator-js": hot(preferDefault(require("/Users/codepreneur/WebstormProjects/Brium-Official/src/pages/fare-estimator.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/codepreneur/WebstormProjects/Brium-Official/src/pages/index.js"))),
