9abeb87dab3965eff7d1d6a43ce50264324948b1
Timi Tejumola <timitejumola@Timis-MacBook-Pro.local>
2019-10-08 12:23:13 +0100

    fix mobile responsiveness on home page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/timitejumola/WebProjects/Brium-Official/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/timitejumola/WebProjects/Brium-Official/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/timitejumola/WebProjects/Brium-Official/src/pages/index.js"))),
   "component---src-pages-signup-js": hot(preferDefault(require("/Users/timitejumola/WebProjects/Brium-Official/src/pages/signup.js"))),
