fb5d52a4667518d44ffa7b56ef1dec1e4d1966a0
Timi Tejumola <tejumoladavid@gmail.com>
2019-10-15 00:57:11 +0100

    implement new page flow

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,14 +2,13 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-corp-signup-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/corp-signup.js" /* webpackChunkName: "component---src-pages-corp-signup-js" */),
   "component---src-pages-corporate-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/corporate.js" /* webpackChunkName: "component---src-pages-corporate-js" */),
   "component---src-pages-index-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-individual-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/individual.js" /* webpackChunkName: "component---src-pages-individual-js" */),
   "component---src-pages-signup-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/signup.js" /* webpackChunkName: "component---src-pages-signup-js" */),
-  "component---src-pages-success-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/success.js" /* webpackChunkName: "component---src-pages-success-js" */),
-  "component---src-pages-corp-signup-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/corp-signup.js" /* webpackChunkName: "component---src-pages-corp-signup-js" */)
+  "component---src-pages-success-js": () => import("/Users/timitejumola/WebProjects/Brium-Official/src/pages/success.js" /* webpackChunkName: "component---src-pages-success-js" */)
 }
 
 exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/timitejumola/WebProjects/Brium-Official/.cache/data.json")
---
+++ public/styles-0c00e41ef30731ebbd4c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{267:function(n,w,o){}}]);
+//# sourceMappingURL=styles-0c00e41ef30731ebbd4c.js.map
